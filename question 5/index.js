f871115012d98936d18ac5c4569c0d3308f7695b
mainaanthony <mainaanthonymwai@gmail.com>
2023-05-12 10:59:29 +0300

    added the answer to question 5

---
+++ question 5/index.js
@@ -0,0 +1,9 @@
+
+
+
+let sum = 5+ 5+"5";
+
+console.log(sum);
+
+
+// We get the output 105 because the last 5 is considered a string thus only the first two numbers are added and the last 5 written as a string "5" thus the result 105
